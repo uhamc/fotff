ea69cabf64bafd54aa48975f564d63ad05518cc4
huangshan <huangshan9@huawei.com>
2022-12-03 10:55:36 +0800

    add http request impl
    Signed-off-by: huangshan <huangshan9@huawei.com>

---
+++ utils/http.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func DoSimpleHttpReq(method string, url string, body []byte) (ret []byte, err error) {
+	for i := 0; i < 3; i++ {
+		if ret, err = doSimpleHttpReqImpl(method, url, body); err == nil {
+			return
+		}
+		time.Sleep(5 * time.Second)
+	}
+	return
+}
+
+func doSimpleHttpReqImpl(method string, url string, body []byte) (ret []byte, err error) {
+	req, err := http.NewRequest(method, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := proxyClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 300 {
+		data, _ := io.ReadAll(resp.Body)
+		if resp.StatusCode == http.StatusProxyAuthRequired || resp.StatusCode == http.StatusForbidden {
+			SwitchProxy()
+		}
+		log.Printf("%s %s: code: %d body: %s", method, url, resp.StatusCode, string(data))
+		return nil, fmt.Errorf("%s %s: code: %d body: %s", method, url, resp.StatusCode, string(data))
+	}
+	return io.ReadAll(resp.Body)
+}
---
+++ utils/proxy.go
@@ -0,0 +1,7 @@
+package utils
+
+import "net/http"
+
+var proxyClient = http.DefaultClient
+
+func SwitchProxy() {}
