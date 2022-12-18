4613da88239b82e262e32eb062c4ccb7578dd26a
huangshan <huangshan9@huawei.com>
2022-12-18 13:54:01 +0800

    remove un-needed files
    Signed-off-by: huangshan <huangshan9@huawei.com>

--- pkg/dayu200/build.go
+++ pkg/dayu200/build.go
@@ -25,6 +25,29 @@ import (
 	"path/filepath"
 )
 
+const (
+	preCompileCMD = `rm -rf prebuilts/clang/ohos/darwin-x86_64/clang-480513;rm -rf prebuilts/clang/ohos/windows-x86_64/clang-480513;rm -rf prebuilts/clang/ohos/linux-x86_64/clang-480513;bash build/prebuilts_download.sh`
+	compileCMD    = `echo 'start' && export NO_DEVTOOL=1 && export CCACHE_LOG_SUFFIX="dayu200-arm32" && export CCACHE_NOHASHDIR="true" && export CCACHE_SLOPPINESS="include_file_ctime" && ./build.sh --product-name rk3568 --ccache --build-target make_all --gn-args enable_notice_collection=false`
+)
+
+var imgList = []string{
+	"out/rk3568/packages/phone/images/MiniLoaderAll.bin",
+	"out/rk3568/packages/phone/images/boot_linux.img",
+	"out/rk3568/packages/phone/images/parameter.txt",
+	"out/rk3568/packages/phone/images/system.img",
+	"out/rk3568/packages/phone/images/uboot.img",
+	"out/rk3568/packages/phone/images/userdata.img",
+	"out/rk3568/packages/phone/images/vendor.img",
+	"out/rk3568/packages/phone/images/resource.img",
+	"out/rk3568/packages/phone/images/config.cfg",
+	"out/rk3568/packages/phone/images/ramdisk.img",
+	// "out/rk3568/packages/phone/images/chipset.img",
+	"out/rk3568/packages/phone/images/sys_prod.img",
+	"out/rk3568/packages/phone/images/chip_prod.img",
+	"out/rk3568/packages/phone/images/updater.img",
+	// "out/rk3568/packages/phone/updater/bin/updater_binary",
+}
+
 func (m *Manager) build(pkg string, ctx context.Context) error {
 	logrus.Infof("now build %s", pkg)
 	server := res.GetBuildServer()
